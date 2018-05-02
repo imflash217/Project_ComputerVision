7fc62f3c73ab9afbbab804b740380c54041c2ce3
vinaykumar2491 <vinaykumar2491@gmail.com>
2018-05-03 02:31:31 +0530

    MAJOR update: kNN/FINISHED

--- modules/classifiers/k_nearest_neighbor.py
+++ modules/classifiers/k_nearest_neighbor.py
@@ -45,11 +45,11 @@ class KNearestNeighbor:
 
         """
         if num_loops == 0:
-            dist_matrix = self.compute_disatances_no_loops(X, distance_metric=distance_metric)
+            dist_matrix = self.compute_distances_no_loops(X, distance_metric=distance_metric)
         elif num_loops == 1:
-            dist_matrix = self.compute_disatances_one_loops(X, distance_metric=distance_metric)
+            dist_matrix = self.compute_distances_one_loops(X, distance_metric=distance_metric)
         elif num_loops == 2:
-            dist_matrix = self.compute_disatances_two_loops(X, distance_metric=distance_metric)
+            dist_matrix = self.compute_distances_two_loops(X, distance_metric=distance_metric)
         else:
             raise ValueError('Invalidvalue %d for num_loops' % num_loops)
         
@@ -57,7 +57,7 @@ class KNearestNeighbor:
 
         
 
-    def compute_disatances_two_loops(self, X, distance_metric=1):
+    def compute_distances_two_loops(self, X, distance_metric=1):
         """
         Inputs:
         - X: Test data; a numpy array of shape (num_test, D) where each num_test test-point is a D-dimensioanl vector
@@ -87,11 +87,110 @@ class KNearestNeighbor:
 
 
 
-    def compute_disatances_one_loops(self, X, distance_metric=1):
-        pass
+    def compute_distances_one_loops(self, X, distance_metric=1):
+        """
+        Inputs:
+        - X: Test data; a numpy array of shape (num_test, D) where each num_test test-point is a D-dimensioanl vector
+        - distance_metric: metric used to calculate the distance b/w the test-point and training-point
+
+        Returns:
+        - dist_matrix: a numpy array of shape (num_test, num_train) 
+                dist_matrix[i,j] is the distance b/w X[i] and self.X_train[j]
+        """
+
+        num_test = X.shape[0]
+        num_train = self.X_train[0]
+        dist_matrix = np.zeros((num_test, num_train))
+
+        for i in range(num_test):
+            if distance_metric == 1:
+                dist_matrix[i] = np.sum(np.abs(self.X_train - X[i,:]), axis=1)      # L1 (Manhattan) Distance
+            elif distance_metric == 2:
+                dist_matrix[i] = np.sqrt(np.sum(np.square(self.X_train - X[i,:]), axis=1))      # L2 (Eucledian) Distance
+            else:
+                raise ValueError('Invalid value %d for distance_metric' % distance_metric)
+        
+        return distance_metric
+
     
-    def compute_disatances_no_loops(self, X, distance_metric=1):
-        pass
+
+    def compute_distances_no_loops(self, X, distance_metric=1):
+        """
+        Inputs:
+        - X: Test data; a numpy array of shape (num_test, D) where each num_test test-point is a D-dimensioanl vector
+        - distance_metric: metric used to calculate the distance b/w the test-point and training-point
+
+        Returns:
+        - dist_matrix: a numpy array of shape (num_test, num_train) 
+                dist_matrix[i,j] is the distance b/w X[i] and self.X_train[j]
+
+        """
+        num_test = X.shape[0]
+        num_train = self.X_train[0]
+        dist_matrix = np.zeros((num_test, num_train))
+
+        if distance_metric == 1:
+            pass
+        elif distance_metric == 2
+            # L2 (Eucledian) Matrix using (a-b)^2 = a^2 + b^2 - 2ab
+            dist_matrix = np.sqrt((-2)*(np.dot(X, self.X_train.T)) + np.sum(np.square(self.X_train), axis=1) + np.transpose([np.sum(np.square(X), axis=1)]))
+        else:
+            raise ValueError('Invalid value %d for distance_metric' % distance_metric)
+        
+        return dist_matrix
     
+
+
     def predict_labels(self, distances, k=1):
-        pass
+        """
+        Predict the labels for the test data.
+
+        Inputs:
+        - distances: a numpy array of shape (num_test, num_train) 
+                where each distances[i,j] represents the distance b/w X_test[i] and X_train[j]
+        - k: the value of 'k' in k-Nearest Neighbor; the number of smallest distances used for voting
+
+        Returns:
+        - Y_pred: a numpy array of shape (num_test,) where Y_pred[i] represnts the PREDICTED LABEL for X_test[i]
+        
+        """
+
+        num_test = distances.shape[0]
+        Y_pred = np.zeros((num_test,))
+
+        
+        for i in range(num_test):
+            # extracting k-nearest-neighbors for each test-point
+            kNN_idxs = np.argsort(distances[i,:])[0:k]
+            
+            # voting among the k-nearest-neighbors
+            kNN_labels = {}
+
+            for j in range(k):
+                m_label = self.Y_train[kNN_idxs[j]]
+                if m_label in kNN_labels.keys():
+                    kNN_labels[m_label] += 1        # increment count
+                else:
+                    kNN_labels[m_label] = 1         # initial count when the label occurs
+            
+            # counting the winning label
+
+            winning_label = kNN_labels.keys()[0]        # initialization
+            
+            for label in kNN_labels.keys():
+                if kNN_labels[label] > kNN_labels[winning_label]:
+                    winning_label = label
+                elif kNN_labels[label] == kNN_labels[winning_label]:
+                    # tie breaker
+                    if label < winning_label:
+                        winning_label = label
+            
+
+            Y_pred[i] = winning_label       # storing winning label for each test-point
+        
+        return Y_pred
+        
+####################################################################################################
+############################### THE END ############################################################
+
+
