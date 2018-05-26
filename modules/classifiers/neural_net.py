3017de537ee5f160b7a2d564ad985afe192fa2f2
vinaykumar2491 <vinaykumar2491@gmail.com>
2018-05-26 20:51:05 +0530

    NeuralNet : Training over mini-batches, learning the hyper params

--- modules/classifiers/neural_net.py
+++ modules/classifiers/neural_net.py
@@ -79,7 +79,7 @@ class TwoLayerNet(object):
 
         ################-------------- FORWARD pass -------------------#####################
         ########    X -> (W1, b1) -> ReLU -> (W2, b2) -> scores/labels/FC   ################
-
+        #
         # perform the Forward pass computing the class scores for the input
         # Store the scores in an array of shape (N, C)
 
@@ -88,8 +88,8 @@ class TwoLayerNet(object):
         scores = scores_h1.dot(W2) + b2                 # last layer scores : layer-2 scores
 
         # check if the targets (i.e. y) are given or not
-        # if y == None:
-        #     return scores       # if the targets are not given then just return the scores
+        if y is None:
+            return scores       # if the targets are not given then just return the scores
 
         # If the targets are given the compute the loass and calculate the gradients
         # compute the SOFTMAX loss
@@ -107,7 +107,7 @@ class TwoLayerNet(object):
         ####################-------------- BACKWARD pass -------------------################################
         ########    layer-2 gradients : grads[layer-2] <- ((W2, b2) <- scores)                  ############
         ########    layer-1 gradients : grads[layer-1] <- ((W1, b1) <- ReLU <- (W2 <- scores))  ############
-
+        #
         # compute the gradients:
         # dL_i/dW = -X_i + sum(X_j * L_i)          (NOTE: symbols interpretable only for one layer; for multiple layers interpret accordingly)
         # dL_i/db = -1 + sum(L_i)
@@ -129,3 +129,101 @@ class TwoLayerNet(object):
         return loss, grads
 
     #########################################################################################
+
+    def train(self, X, y, X_val, y_val, 
+                learning_rate=1e-3, learning_rate_decay=0.95, reg=5e-6, 
+                num_iters=100, batch_size=200, verbose=False):
+        
+        """
+        Trains the Neural network using Stocastic Gradient Descent (SGD).
+
+        Inputs:
+        - X : Training data; numpy array of shape (N, D)
+        - y : Training labels; numpy array of shape (N, )
+                0 <= y[i] < C;  y[i] is the label for X[i]
+        - X_val : Validation data; numpy array of shape (V_val, D)
+        - y_val : Validation labels; numpy array of shape (N_val, )
+        - learning_rate : learning rate
+        - learning_rate_decay : used to decay the learning rate after each epoch
+        - reg : Regularization Strength
+        - num_iters : number of steps to take when optimizing
+        - batch_size : numbe rof trainig examples to use per step
+        - verbose : boolean; prints the progress during optimization if True
+
+        Returns:
+        - a dictionary:
+            {'loss_history' : loss_history,
+             'train_acc_history' : train_acc_history,
+             'val_acc_history' : val_acc_history
+            }
+        where:
+            - loss_history : loss for each iteration
+            - train_acc_history : training accuracies for each epoch
+            - val_Acc_history : validation accuracies for eac epoch
+
+        """
+
+        N, D = X.shape
+        iters_per_epoch = max(N/batch_size, 1)
+
+        # SGD to optimize W1, W2, b1, b2 in self.params
+        loss_history = []
+        train_acc_history = []
+        val_acc_history = []
+
+        for it in range(num_iters):
+            batch_idxs = np.random.choice(N, batch_size, replace=True)
+            X_batch = X[batch_idxs, :]      # shape : (batch_size, D)
+            y_batch = y[batch_idxs]         # shape : (batch_size, )
+
+            # calculate loss and gradients for this minibatch for this iteration
+            loss, grads = self.loss(X=X_batch, y=y_batch, reg=reg)
+            loss_history.append(loss)
+
+            # Use gradients to update Weights and biases (W1, W2, b1, b2) using SGD
+            for key in self.params.keys():
+                self.params[key] += -1*learning_rate*grads[key]
+            
+            if verbose:
+                print('iteration [%d / %d] : loss = %f' % (it, num_iters, loss))
+
+            # every epoch check train and val accuracy and decay the learning rate
+            if it % iters_per_epoch == 0:
+                # check the accuraies of train and val sets
+                train_acc = np.mean(self.predict(X_batch) == y_batch)
+                val_acc = np.mean(self.predict(X_val) == y_val)
+                train_acc_history.append(train_acc)
+                val_acc_history.append(val_acc)
+
+                # decay the learning rate
+                learning_rate *= learning_rate_decay
+
+            
+        return {'loss_history' : loss_history,
+                'train_acc_history' : train_acc_history,
+                'val_acc_history' : val_acc_history
+                }
+            
+    ##############################################################################
+
+    def predict(self, X):
+        """
+        Uses the trained weights of this Neural Network to predict labels for the data-points.
+        Step-1: Compute score for each of the 'C' classes for every X[i]
+        Step-2: Assign the class with highest score as label to corresponding X[i]
+
+        Inputs:
+        - X : numpy array of shape (N, D). The data for which we want to predict the labels
+
+        Returns:
+        - y_pred : a numpy array of shape (N, ). It stores the predicted labels for each data-point in X.
+                0 <= y_pred[i] < C;  where y_pred[i] is the predicted label for X[i]
+
+        """
+
+        scores = self.loss(X)               # obtain the scores for the data
+        y_pred = np.argmax(scores, axis=1)   # get the labels for each X[i] corresponding to the highest score
+        return y_pred
+
+
+    ##############################################################################
